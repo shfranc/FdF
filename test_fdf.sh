ea6b92bac0ef2e128f84c553663cf6a5e12f25b9
Sarah FRANC <sfranc@e2r5p17.42.fr>
2017-05-31 12:03:57 +0200

    ajout tests dans le script de test

--- test_fdf.sh
+++ test_fdf.sh
@@ -9,36 +9,36 @@ echo "\033[0;35m--> map que des espaces : \033[00m" && ./fdf maps/maps_onlyspace
 echo "\033[0;35m--> une lettre en hauteur : \033[00m" && ./fdf maps/pars_data.fdf
 echo "\033[0;35m--> format couleur : \033[00m" && ./fdf maps/pars_color.fdf
 
-# echo "\033[01;32mTests basiques\033[00m"
-# ./fdf maps/plat.fdf
-# ./fdf maps/basictest.fdf
-# ./fdf maps/pnp_flat.fdf
-# ./fdf maps/42.fdf
-# ./fdf maps/pentenegpos.fdf
+echo "\033[01;32mTests basiques\033[00m"
+./fdf maps/plat.fdf
+./fdf maps/basictest.fdf
+./fdf maps/pnp_flat.fdf
+./fdf maps/42.fdf
+./fdf maps/pentenegpos.fdf
 
-# echo "\033[01;32mTests moyens\033[00m"
-# ./fdf maps/10-2.fdf
-# ./fdf maps/50-4.fdf
-# ./fdf maps/pyra.fdf
-# ./fdf maps/elem.fdf
+echo "\033[01;32mTests moyens\033[00m"
+./fdf maps/10-2.fdf
+./fdf maps/50-4.fdf
+./fdf maps/pyra.fdf
+./fdf maps/elem.fdf
 
-# echo "\033[01;32mTests grande map\033[00m"
-# ./fdf maps/100-6.fdf
-# ./fdf maps/mars.fdf
+echo "\033[01;32mTests grande map\033[00m"
+./fdf maps/100-6.fdf
+./fdf maps/mars.fdf
 
-# echo "\033[01;32mTests altitude\033[00m"
-# ./fdf maps/10-70.fdf
-# ./fdf maps/20-60.fdf
-# ./fdf maps/elem2.fdf
+echo "\033[01;32mTests altitude\033[00m"
+./fdf maps/10-70.fdf
+./fdf maps/20-60.fdf
+./fdf maps/elem2.fdf
 
-# echo "\033[01;32mTests couleurs\033[00m"
-# ./fdf maps/pyramide.fdf
-# ./fdf maps/elem-col.fdf
+echo "\033[01;32mTests couleurs\033[00m"
+./fdf maps/pyramide.fdf
+./fdf maps/elem-col.fdf
 
-# echo "\033[01;32mTests couleurs et altitude\033[00m"
-# ./fdf maps/t1.fdf
-# ./fdf maps/t2.fdf
+echo "\033[01;32mTests couleurs et altitude\033[00m"
+./fdf maps/t1.fdf
+./fdf maps/t2.fdf
 
-# echo "\033[01;32mTests fractales\033[00m"
-# ./fdf maps/julia.fdf
-# ./fdf maps/elem-fract.fdf
+echo "\033[01;32mTests fractales\033[00m"
+./fdf maps/julia.fdf
+./fdf maps/elem-fract.fdf
