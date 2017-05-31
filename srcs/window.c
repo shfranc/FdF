61932c7440475cca7c70831e1a14eba3c549f7f2
Sarah FRANC <sfranc@e2r5p17.42.fr>
2017-05-31 13:09:33 +0200

    norme

--- srcs/window.c
+++ srcs/window.c
@@ -6,26 +6,45 @@
 /*   By: sfranc <sfranc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/31 12:21:02 by sfranc            #+#    #+#             */
-/*   Updated: 2017/05/31 12:59:43 by sfranc           ###   ########.fr       */
+/*   Updated: 2017/05/31 13:09:09 by sfranc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	ft_win_display_menu(t_holder *hold)
+static void		ft_win_display_menu2(t_holder *hold)
 {
-	mlx_string_put(hold->mlx, hold->win, hold->draw.img_width + 10, 5, 0x00FFFF, "Keys:");
-	mlx_string_put(hold->mlx, hold->win, hold->draw.img_width + 10, 25, 0xFFFFFF, "8 or ^: up");
-	mlx_string_put(hold->mlx, hold->win, hold->draw.img_width + 10, 45, 0xFFFFFF, "2 or v: down");
-	mlx_string_put(hold->mlx, hold->win, hold->draw.img_width + 10, 65, 0xFFFFFF, "6 or >: right");
-	mlx_string_put(hold->mlx, hold->win, hold->draw.img_width + 10, 85, 0xFFFFFF, "4 or <: left");
-	mlx_string_put(hold->mlx, hold->win, hold->draw.img_width + 10, 105, 0xFFFFFF, "7: up & left");
-	mlx_string_put(hold->mlx, hold->win, hold->draw.img_width + 10, 125, 0xFFFFFF, "9: up & right");
-	mlx_string_put(hold->mlx, hold->win, hold->draw.img_width + 10, 145, 0xFFFFFF, "1: down & left");
-	mlx_string_put(hold->mlx, hold->win, hold->draw.img_width + 10, 165, 0xFFFFFF, "3: down & right");
-	mlx_string_put(hold->mlx, hold->win, hold->draw.img_width + 10, 185, 0xFF0000, "r: red");
-	mlx_string_put(hold->mlx, hold->win, hold->draw.img_width + 100, 185, 0x00FF00, "g: green");
-	mlx_string_put(hold->mlx, hold->win, hold->draw.img_width + 10, 205, 0x0000FF, "b: blue");
-	mlx_string_put(hold->mlx, hold->win, hold->draw.img_width + 100, 205, 0xFFFF00, "y: yellow");
-	mlx_string_put(hold->mlx, hold->win, hold->draw.img_width + 10, 225, 0xFFFFFF, "5 or space: RESET");
+	mlx_string_put(hold->mlx, hold->win, hold->draw.img_width + 10, 185,\
+		0xFF0000, "r: red");
+	mlx_string_put(hold->mlx, hold->win, hold->draw.img_width + 100, 185,\
+		0x00FF00, "g: green");
+	mlx_string_put(hold->mlx, hold->win, hold->draw.img_width + 10, 205,\
+		0x0000FF, "b: blue");
+	mlx_string_put(hold->mlx, hold->win, hold->draw.img_width + 100, 205,\
+		0xFFFF00, "y: yellow");
+	mlx_string_put(hold->mlx, hold->win, hold->draw.img_width + 10, 225,\
+		0xFFFFFF, "5 or space: RESET");
+}
+
+void			ft_win_display_menu(t_holder *hold)
+{
+	mlx_string_put(hold->mlx, hold->win, hold->draw.img_width + 10, 5,\
+		0x00FFFF, "Keys:");
+	mlx_string_put(hold->mlx, hold->win, hold->draw.img_width + 10, 25,\
+		0xFFFFFF, "8 or ^: up");
+	mlx_string_put(hold->mlx, hold->win, hold->draw.img_width + 10, 45,\
+		0xFFFFFF, "2 or v: down");
+	mlx_string_put(hold->mlx, hold->win, hold->draw.img_width + 10, 65,\
+		0xFFFFFF, "6 or >: right");
+	mlx_string_put(hold->mlx, hold->win, hold->draw.img_width + 10, 85,\
+		0xFFFFFF, "4 or <: left");
+	mlx_string_put(hold->mlx, hold->win, hold->draw.img_width + 10, 105,\
+		0xFFFFFF, "7: up & left");
+	mlx_string_put(hold->mlx, hold->win, hold->draw.img_width + 10, 125,\
+		0xFFFFFF, "9: up & right");
+	mlx_string_put(hold->mlx, hold->win, hold->draw.img_width + 10, 145,\
+		0xFFFFFF, "1: down & left");
+	mlx_string_put(hold->mlx, hold->win, hold->draw.img_width + 10, 165,\
+		0xFFFFFF, "3: down & right");
+	ft_win_display_menu2(hold);
 }
